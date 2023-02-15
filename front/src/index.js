9fc37f65cc4fffd12b6457ce37577ecd17db4129
taran2003 <taran2003@bk.ru>
2023-02-15 12:51:47 +0300

    axios & react-router-dom import

--- front/src/index.js
+++ front/src/index.js
@@ -1,32 +1,18 @@
-import './CSS/index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from './Routes/Login';
-import Register from './Routes/Register';
-import Main from './Routes/Main';
-import Comment from './Routes/Comments';
-import User from './Routes/User';
-import PrivateRoute from './Routes/PrivateRoute';
-import AuthProvider from './Providers/AuthProvider';
-import PostProvider from './Providers/PostProvider';
-import CommentProvider from './Providers/CommentProvider';
-import lab3 from './Lab3';
-import Logout from "./Routes/Logout";
-import PostCreate from './Routes/AddPost';
+import Login from './routes/login/page';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <React.StrictMode>
-        <AuthProvider>
             <BrowserRouter>
                 <Routes>
                     <Route path="/login" element={<Login />} />
-                    <Route path="/register" element={<Register />} />
-                    <Route path="/chat" element={<Logout />} />
+                    {/* <Route path="/register" element={<Register />} />
+                    <Route path="/chat" element={<Logout />} /> */}
                 </Routes>
             </BrowserRouter>
-        </AuthProvider>
     </React.StrictMode>
 );
--- front/src/routes/login/page.js
+++ front/src/routes/login/page.js
@@ -1,14 +1,13 @@
-import '../CSS/Login.css';
+import { formToJSON } from "axios";
 import { useState } from "react";
-import useAuthContext from '../Providers/useAuthContext';
-import NavigationComponent from "../Components/NavigationComponent";
-import Form from "../Components/LoginFormComponent";
-import { login, getUserFromStorage} from "../Helper/ServerRequest";
-import { useNavigate } from "react-router-dom";
+// import useAuthContext from '../Providers/useAuthContext';
+// import NavigationComponent from "../Components/NavigationComponent";
+// import { login, getUserFromStorage} from "../Helper/ServerRequest";
+// import { useNavigate } from "react-router-dom";
 
 function Login() {
-    const navigate = useNavigate();
-    const { setIsLoggedIn, setUser} = useAuthContext();
+    // const navigate = useNavigate();
+    // const { setIsLoggedIn, setUser} = useAuthContext();
 
     let [form, setForm] = useState({
         login: '', password: '',
@@ -18,21 +17,23 @@ function Login() {
         setForm({ ...form, [e.target.name]: e.target.value });
     }
 
-    async function onSubmit() {
-        await login({ login: form.login, password: form.password });
-        const { firstName, lastName } = getUserFromStorage();
-        setIsLoggedIn(true);
-        navigate("/main");
+    function onSubmit() {
+        // await login({ login: form.login, password: form.password });
+        // const { firstName, lastName } = getUserFromStorage();
+        // setIsLoggedIn(true);
+        // navigate("/main");
+        console.log(form);
+        return false;
     }
 
     return (<div>
-        <NavigationComponent>register</NavigationComponent>
+        {/* <NavigationComponent>register</NavigationComponent> */}
         <div className="App">
-            <Form
-                onChange={handleState}
-                onSubmit={onSubmit}
-                form={form}
-            />
+            <form onSubmit={onSubmit}>
+                <input type="text" onChange={handleState} placeholder={"Login"}></input>
+                <input type="password" onChange={handleState} placeholder={"password"}></input>
+                <input type="submit"></input>
+            </form>
         </div>
     </div>);
 }
